8e408bb6ec22e5335a970a95772a01bcf4da8bf2
Alejandro Aguilar <downvieri@gmail.com>
2016-07-21 02:59:32 -0500

    Cambios
    Nueva clase

--- app/src/main/java/com/alexaguilar/miscontactos/ContactoAdaptador.java
+++ app/src/main/java/com/alexaguilar/miscontactos/ContactoAdaptador.java
@@ -2,6 +2,8 @@ package com.alexaguilar.miscontactos;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Alex on 21/07/16.
@@ -10,8 +12,14 @@ public class ContactoAdaptador {
 
     public static class ContactoViewHolder extends RecyclerView.ViewHolder{
 
+        private ImageView imgFoto;
+        private TextView tvNombreCV;
+        private TextView tvTelenoCV;
+
         public ContactoViewHolder(View itemView) {
             super(itemView);
+
+            imgFoto = (ImageView) itemView.findViewById(R.id.imgFoto);
         }
     }
 

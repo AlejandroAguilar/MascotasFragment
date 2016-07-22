2a0628e0b36bc3d46d3f208c45d5d6d26c09673d
Alejandro Aguilar <downvieri@gmail.com>
2016-07-22 02:04:50 -0500

    Cambios en la agencia
    Cambios en la agencia

--- app/src/main/java/com/alexaguilar/miscontactos/ContactoAdaptador.java
+++ app/src/main/java/com/alexaguilar/miscontactos/ContactoAdaptador.java
@@ -1,14 +1,47 @@
 package com.alexaguilar.miscontactos;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Alex on 21/07/16.
  */
-public class ContactoAdaptador {
+public class ContactoAdaptador extends RecyclerView.Adapter<ContactoAdaptador.ContactoViewHolder>{
+
+    ArrayList<Contacto> contactos;
+
+    public ContactoAdaptador(ArrayList<Contacto> contactos){
+        this.contactos = contactos;
+    }
+
+
+    // Inflar el layout y lo pasara al viewholder para que obtenga los views
+    @Override
+    public ContactoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_contacto,parent,false);
+        return new ContactoViewHolder(v);
+    }
+
+    // asocia cada elemento de la lista con cada view
+    @Override
+    public void onBindViewHolder(ContactoViewHolder contactoViewHolderholder, int position) {
+        Contacto contacto = contactos.get(position);
+        contactoViewHolderholder.imgFoto.setImageResource(contacto.getFoto());
+        contactoViewHolderholder.tvNombreCV.setText(contacto.getNombre());
+        contactoViewHolderholder.tvTelenoCV.setText(contacto.getTelefono());
+
+    }
+
+    @Override
+    public int getItemCount() {   // cantidad de elemenos que contiene mi lista
+        return contactos.size();
+    }
 
     public static class ContactoViewHolder extends RecyclerView.ViewHolder{
 
@@ -20,6 +53,9 @@ public class ContactoAdaptador {
             super(itemView);
 
             imgFoto = (ImageView) itemView.findViewById(R.id.imgFoto);
+            tvNombreCV = (TextView) itemView.findViewById(R.id.tvNombreCV);
+            tvTelenoCV = (TextView) itemView.findViewById(R.id.tvTelefonoCV);
+
         }
     }
 
--- app/src/main/java/com/alexaguilar/miscontactos/MainActivity.java
+++ app/src/main/java/com/alexaguilar/miscontactos/MainActivity.java
@@ -3,32 +3,50 @@ package com.alexaguilar.miscontactos;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Contacto> contactos;
+    private RecyclerView listaContactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        contactos = new ArrayList<Contacto>();
 
-        contactos.add(new Contacto(R.drawable.user1,"Alejandro Aguilar","99998888","downvieri@gmail.com"));
-        contactos.add(new Contacto(R.drawable.user2,"Pedro Sanchez","22298888","pedro@gmail.com"));
-        contactos.add(new Contacto(R.drawable.user3,"Ana Perez","11198888","ana@gmail.com"));
-        contactos.add(new Contacto(R.drawable.user4,"Luis Cabrera","00098888","luis@gmail.com"));
+        //Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        //setSupportActionBar(miActionBar);
 
+        listaContactos = (RecyclerView) findViewById(R.id.rvContactos);
+
+        // LinearLayoutManager llm = new LinearLayoutManager(this);
+        // llm.setOrientation(LinearLayoutManager.VERTICAL);
+        // listaContactos.setLayoutManager(llm);
+
+        GridLayoutManager glm = new GridLayoutManager(this, 2);
+        listaContactos.setLayoutManager(glm);
+
+
+
+        inicializarListaContactos();
+
+
+        /*
         ArrayList<String> nombresContacto = new ArrayList<>();
         for (Contacto contacto: contactos) {
             nombresContacto.add(contacto.getNombre());
         }
+        */
 
         /*
         ListView lstContactos = (ListView) findViewById(R.id.lstContactos);
@@ -48,4 +66,19 @@ public class MainActivity extends AppCompatActivity {
         */
 
     }
+
+    public void inicializarAdaptador(){
+        ContactoAdaptador adaptador = new ContactoAdaptador(contactos);
+        listaContactos.setAdapter(adaptador);
+    }
+
+    public void inicializarListaContactos(){
+        contactos = new ArrayList<Contacto>();
+
+        contactos.add(new Contacto(R.drawable.user1,"Alejandro Aguilar","99998888","downvieri@gmail.com"));
+        contactos.add(new Contacto(R.drawable.user2,"Pedro Sanchez","22298888","pedro@gmail.com"));
+        contactos.add(new Contacto(R.drawable.user3,"Ana Perez","11198888","ana@gmail.com"));
+        contactos.add(new Contacto(R.drawable.user4,"Luis Cabrera","00098888","luis@gmail.com"));
+    }
+
 }

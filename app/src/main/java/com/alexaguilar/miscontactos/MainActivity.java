e7737367073ed7b6c96428243f92c95da48397f8
Alejandro Aguilar <downvieri@gmail.com>
2016-07-26 00:58:44 -0500

    Con favoritos
    Con favoritos

--- app/src/main/java/com/alexaguilar/miscontactos/MainActivity.java
+++ app/src/main/java/com/alexaguilar/miscontactos/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -93,8 +95,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void fabClicked(View v){
-        // write your code here ..
         Toast.makeText(this, "Subir", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbarmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_favorite:
+                Intent intent=new Intent(this, FavoritosActivity.class);
+                this.startActivity(intent);
+                return true;
+            case R.id.action_about:
+                Toast.makeText(this,"Thank you",Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }

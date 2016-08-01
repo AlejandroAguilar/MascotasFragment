2413c7149577f8bb3e26d0ea24f210ffaf29cef2
Alejandro Aguilar <downvieri@gmail.com>
2016-07-31 23:50:37 -0500

    Email
    Email

--- app/src/main/java/com/alexaguilar/miscontactos/ContactoActivity.java
+++ app/src/main/java/com/alexaguilar/miscontactos/ContactoActivity.java
@@ -3,8 +3,25 @@ package com.alexaguilar.miscontactos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Properties;
+
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 
 public class ContactoActivity extends AppCompatActivity {
+
+    private Button buttonEnviar;
+    private EditText TextMail;
+    private EditText TextAsunto;
+    private EditText TextMensaje;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +38,27 @@ public class ContactoActivity extends AppCompatActivity {
         getSupportActionBar().setLogo(R.drawable.huella);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
 
+    }
+
+    /*
+    public void fabClicked(View v){
+        Toast.makeText(this, "Subir", Toast.LENGTH_SHORT).show();
+    }
+    */
+
+    private void sendEmail(View v) {
+
+        Toast.makeText(this, "Email!", Toast.LENGTH_SHORT).show();
+
+        //Getting content for email
+        String email = TextMail.getText().toString().trim();
+        String subject = TextAsunto.getText().toString().trim();
+        String message = TextMensaje.getText().toString().trim();
+
+        //Creating SendMail object
+        SendMail sm = new SendMail(this, email, subject, message);
+
+        //Executing sendmail to send email
+        sm.execute();
     }
 }
